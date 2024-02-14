162ea6f23d3d3ec1486cd694f719ff1c8382aa79
Hacker-Code-J <hacker3740@gmail.com>
2024-02-14 17:51:06 +0900

    Laptop240214v1.2

--- include/secp256k1_bigint.h
+++ include/secp256k1_bigint.h
@@ -4,6 +4,6 @@
 #ifndef _SECP256K1_BIGINT_H
 #define _SECP256K1_BIGINT_H
 
-void add_core(u32* result, const u32 op1[8], const u32 op2[8]);
+void add_core(word* result, const u32 op1[SIZE], const u32 op2[SIZE]);
 
 #endif /* _SECP256K1_BIGINT_H */
