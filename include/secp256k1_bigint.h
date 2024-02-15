8479e3281e0b5537b132ff168c162d6375cceeff
Hacker-Code-J <hacker3740@gmail.com>
2024-02-16 03:36:53 +0900

    Laptop240216v1

--- include/secp256k1_bigint.h
+++ include/secp256k1_bigint.h
@@ -1,9 +1,30 @@
 #include "secp256k1_config.h"
 
-
 #ifndef _SECP256K1_BIGINT_H
 #define _SECP256K1_BIGINT_H
 
-void add_core(word* result, const u32 op1[SIZE], const u32 op2[SIZE]);
+#ifdef IS_32_BIT_ENV
+static const prime_field PRIME = {
+	0xFFFFFC2FU, 0xFFFFFFFEU, 0xFFFFFFFFU, 0xFFFFFFFFU,
+	0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU
+};
+static const prime_field PRIME_COMPLEMENT = {
+	0x000003D0U, 0x00000001U, 0x00000000U, 0x00000000U,
+	0x00000000U, 0x00000000U, 0x00000000U, 0x00000000U
+};
+#else
+static const prime_field PRIME = {
+	0xFFFFFFFEFFFFFC2FU, 0xFFFFFFFFFFFFFFFFU,
+	0xFFFFFFFFFFFFFFFFU, 0xFFFFFFFFFFFFFFFFU
+};
+static const prime_field PRIME_COMPLEMENT = {
+	0x00000001000003D0U, 0x0000000000000000U,
+	0x0000000000000000U, 0x0000000000000000U
+};
+#endif
+
+void add_core(bool* pEpsilon, word result[SIZE], const word op1[SIZE], const word op2[SIZE]);
+void addition_p256(word result[SIZE], const word op1[SIZE], const word op2[SIZE]);
+
 
 #endif /* _SECP256K1_BIGINT_H */
